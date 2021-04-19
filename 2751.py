286d7f6ecc2040de96305473122458e7937213ac
Center <a52675268@naver.com>
2021-04-19 13:34:41 +0900

    complete merge sorting in 2751

--- 2751.py
+++ 2751.py
@@ -1,5 +1,41 @@
 #정렬
 #O(n*lgn)으로 풀어보기
+#합병정렬으로 풀기
+
+def usersort(n_list):
+
+    if len(n_list) == 1:
+        return n_list
+
+    mid = len(n_list) // 2
+    
+    front_list = usersort(n_list[:mid])
+    back_list = usersort(n_list[mid:])
+    
+    sorted_list = merge(front_list,back_list)
+    
+    return sorted_list
+
+def merge(list1,list2):
+
+    merge_list = []
+
+    while (len(list1) != 0) and (len(list2) != 0):
+        if list1[0] < list2[0]:
+            merge_list.append(list1[0])
+            list1.pop(0)
+        else:
+            merge_list.append(list2[0])
+            list2.pop(0)
+
+
+    if len(list1) == 0:
+        merge_list += list2
+    else:
+        merge_list += list1
+
+    return merge_list
+
 
 n = int(input())
 num_list = []
@@ -7,7 +43,6 @@ num_list = []
 for _ in range(n):
     num_list.append(int(input()))
 
-num_list.sort()
+sort_list = usersort(num_list)
 
-for _ in range(n):
-    print(num_list[_])
+print(sort_list)
