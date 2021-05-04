46b5d55a03a3ea87ee7c70f7dd3226dfc6af70ed
Center <a52675268@gmail.com>
2021-05-04 13:37:14 +0900

    complete solving part-sum problem

--- DynamicProgramming/1912.py
+++ DynamicProgramming/1912.py
@@ -4,27 +4,10 @@ import sys
 
 n = int(sys.stdin.readline().strip())
 num_list = list(map(int,sys.stdin.readline().split()))
+sum_list = [0]*n
+sum_list[0] = num_list[0]
 
-result = num_list[0]
-a,b,c = 0, 0, 0 #총합, 양수
+for i in range(n-1):
+    sum_list[i+1] = max(sum_list[i]+num_list[i+1],num_list[i+1])
 
-for i in num_list:
-    if i >= 0:
-        if b == 0:
-            a += i
-        else:
-            c += i
-    else:
-        if c == 0:
-            b += i
-        else:
-            result = max(result,a,c,a+b+c)
-            a = max(c,a+b+c)
-            c = 0
-            b = i
-    print(a,b,c,result)
-
-if a==0:
-    print(result)
-else:
-    print(max(result,a,c,a+b+c))
+print(max(sum_list))
