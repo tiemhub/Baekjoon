f88d8336c123d6860d1b8c29b2a65f975b28b5e5
Center <a52675268@gmail.com>
2021-05-06 11:54:18 +0900

    faced with limitation of my kanpsack algorithm

--- DynamicProgramming/12865.py
+++ DynamicProgramming/12865.py
@@ -6,4 +6,19 @@ n, k = map(int,sys.stdin.readline().split())
 things = [[0,0] for _ in range(n)]
 
 for i in range(n):
-    things[i] = list(map(int,sys.stdin.readline.split()))
+    things[i] = list(map(int,sys.stdin.readline().split()))
+
+things.sort(reverse = True, key = lambda x : (x[1],x[0]))
+cache = []
+isin = False
+for i in things:
+    for j in cache:
+        if i[0] + j[0] <= k:
+            cache.append([i[0]+j[0],i[1]+j[1]])
+            isin = True
+    if not isin:
+        cache.append(i)
+    print(cache)
+        
+
+print(max(cache,key = lambda x : x[1])[1])
