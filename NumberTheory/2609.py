1aa8df53333648ee2263749669d95ed536cc3fc6
Center <a52675268@gmail.com>
2021-05-10 15:19:18 +0900

    solve GCD and LCM

---
+++ NumberTheory/2609.py
@@ -0,0 +1,17 @@
+#정수론
+#최소공배수
+import sys
+
+for _ in range(int(sys.stdin.readline().strip())):
+    a, b = map(int,sys.stdin.readline().split())
+    gcd = 0
+    lcm = 0
+
+    for i in range(a,0,-1):
+        if a%i == 0 and b%i == 0:
+            gcd = i
+            break
+
+    lcm = a * b // gcd
+
+    print(lcm)
