4a84a963e907a5f00ccfdcfb5764be2bc8e07b30
Center <a52675268@gmail.com>
2021-05-31 14:59:31 +0900

    finishi to solve binary searchproblem(1 left)

--- BinarySearch/12015.py
+++ BinarySearch/12015.py
@@ -1,15 +1,32 @@
 #이분탐색
 #가장 긴 증가하는 부분 순열
+#동적계획법 이용
+#10 20 30 70 과 10 20 50 70 수열은 결국 길이가 같다
+#이분탐색 실패시 dp 증가, 성공시 값을 바꾼다 => 바꿔도 길이가 변하지는 않는다.
 import sys
 
 n = int(sys.stdin.readline().strip())
 li = list(map(int,sys.stdin.readline().split()))
 
-start, end = 1, n-1
+dp = [-1]*n
+flag = 0
 
-while start <= end:
+for i in li:
 
+    start, end = 0, flag
     
-    mid = (start + end) // 2
+    while start <= end:
 
-    
+        mid = (start + end) // 2
+        
+        if i > dp[mid]:
+            start = mid +1
+        elif i < dp[mid]:
+            end = mid - 1
+
+    if end >= n:
+        flag += 1
+    dp[end] = i
+    print(dp)
+
+print(len([i for i in dp if (i+1)]))
