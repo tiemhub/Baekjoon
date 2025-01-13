679a1e28e58132161395b9455c0451889fdd81bf
TiEM <52905720+tiemhub@users.noreply.github.com>
2025-01-13 12:39:58 +0900

    14503 최적화 고민해보기

--- 14503.py
+++ 14503.py
@@ -10,21 +10,33 @@
   1. 반시계 방향으로 90도 회전한다.
   2. 바라보는 방향을 기준으로 앞쪽 칸이 청소되지 않은 빈 칸인 경우 한 칸 전진한다.
   3. 1번으로 돌아간다.
-
+2,3번 통합 가능 => 현재 자리에서 청소되지 않은 빈칸 방향을 return 해주기
 """
 import sys
+
+def checkclean(r,c):
+    check = True
+
+    if (r != 0):
+        if maps[r-1][c] == 0:
+
 
 n, m = map(int,sys.stdin.readline().split())
 r, c, d = map(int, sys.stdin.readline().split())
 
-visited = [[False] * m] * n
+# 0 => 청소되지 않음, 1 => 벽, 2 => 청소됨
 maps = []
 
 for _ in range(n):
     maps.append(list(map(int, sys.stdin.readline().split())))
 
-ans = 0
+cnt = 0
 
 while True:
 
-    if maps[r][c]
+    # 청소되어 있지 않으면 청소하기
+    if maps[r][c] == 0:
+        maps[r][c] = 2
+        cnt += 1
+    
+    if checkclean(r,c):
