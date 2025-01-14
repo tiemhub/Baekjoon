d966a7e71373ab45e23c62991d4d658e3aeb57c7
TiEM <52905720+tiemhub@users.noreply.github.com>
2025-01-14 12:49:13 +0900

    14503 check moving logic

--- 14503.py
+++ 14503.py
@@ -14,11 +14,25 @@
 """
 import sys
 
-def checkclean(r,c):
-    check = True
+def canmove(r,c,d):
+    d = (d-1)%4
 
-    if (r != 0):
-        if maps[r-1][c] == 0:
+    for i in range(4):
+        if d==0 and r != 0: #이동방향이 북쪽이고, 제일 위가 아닐때
+            if maps[r-1][c] == 0: #북쪽 타일이 청소하지 않았다면
+                return d #이동방향은 북쪽
+        if d==1 and (c != len(maps[0])-1): #이동방향이 동쪽이고
+            if maps[r][c+1] == 0:
+                return d
+        if d==2 and (r != len(maps)-1):
+            if maps[r+1][c] == 0:
+                return d
+        if d==3 and (c != 0):
+            if maps[r][c-1] == 0:
+                return d
+        d = (d-1)%4
+    
+    return 5
 
 
 n, m = map(int,sys.stdin.readline().split())
@@ -31,12 +45,47 @@ for _ in range(n):
     maps.append(list(map(int, sys.stdin.readline().split())))
 
 cnt = 0
+end = False
 
-while True:
-
+while not end:
+    print(r,c,d,cnt)
     # 청소되어 있지 않으면 청소하기
     if maps[r][c] == 0:
         maps[r][c] = 2
         cnt += 1
     
-    if checkclean(r,c):
+    direction = canmove(r,c,d)
+    print(direction)
+    if direction == 0:
+        r, c, d = r-1, c, direction
+    elif d == 1:
+        r, c, d = r, c+1, direction
+    elif d == 2:
+        r, c, d = r+1, c, direction
+    elif d == 3:
+        r, c, d = r, c-1, direction
+    else: #만약 주변에 빈 칸이 없다면
+        if d == 0:
+            if (r != (len(maps)-1)) and (maps[r+1][c] != 1):
+                r, c = r+1, c
+            else:
+                end = True
+        elif d == 1:
+            if (c != 0) and (maps[r][c-1] != 1):
+                r, c = r, c-1
+            else:
+                end = True
+        elif d == 2:
+            if (r != 0) and (maps[r-1][c] != 1):
+                r, c = r-1, c
+            else:
+                end = True
+        elif d == 3:
+            if (c != (len(maps[0])-1)) and (maps[r][c+1] != 1):
+                r, c = r, c+1
+            else:
+                end = True
+
+print(cnt)
+
+
